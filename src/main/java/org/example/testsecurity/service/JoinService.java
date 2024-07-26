568a00d853c745d57275ef8ac85d9aedff14ff32
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:23:07 +0900

    add joinService

--- src/main/java/org/example/testsecurity/service/JoinService.java
+++ src/main/java/org/example/testsecurity/service/JoinService.java
@@ -1,2 +1,32 @@
-package org.example.testsecurity.service;public class JoinService {
+package org.example.testsecurity.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.testsecurity.dto.JoinDTO;
+import org.example.testsecurity.entity.UserEntity;
+import org.example.testsecurity.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO) {
+        boolean isUser = userRepository.existByUsername(joinDTO.getUsername());
+        if (isUser) {
+            return;
+        }
+
+        UserEntity entity = new UserEntity();
+
+        entity.setUsername(joinDTO.getUsername());
+        entity.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+        entity.setRole("ROLE_USER");
+
+        userRepository.save(entity);
+    }
 }
