c88efad79a7eb139c4cc5aafb286d9a2371ff1d4
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:23:38 +0900

    add SecurityConfig

--- src/main/java/org/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/org/example/testsecurity/config/SecurityConfig.java
@@ -1,2 +1,46 @@
-package org.example.testsecurity.config;public class SecurityConfig {
+package org.example.testsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .formLogin((auth) -> auth
+                        .loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                );
+
+//        http
+//                .csrf((auth) -> auth.disable());
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionFixation()
+                        .changeSessionId()
+                );
+
+        return http.build();
+    }
 }
