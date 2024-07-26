959b66099b4b89dc4e0d6b61c219c1c85b7809c6
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:23:00 +0900

    add joinDto

--- src/main/java/org/example/testsecurity/dto/JoinDTO.java
+++ src/main/java/org/example/testsecurity/dto/JoinDTO.java
@@ -1,2 +1,11 @@
-package org.example.testsecurity.dto;public class JoinDTO {
+package org.example.testsecurity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class JoinDTO {
+    private String username;
+    private String password;
 }
