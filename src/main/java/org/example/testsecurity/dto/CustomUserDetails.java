e0c82146922a3a6b6c23754fb880b422b5ccebd8
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:22:08 +0900

    add CustomUserDetails

--- src/main/java/org/example/testsecurity/dto/CustomUserDetails.java
+++ src/main/java/org/example/testsecurity/dto/CustomUserDetails.java
@@ -1,2 +1,65 @@
-package org.example.testsecurity.dto;public class CustomUserDetails {
+package org.example.testsecurity.dto;
+
+
+import org.example.testsecurity.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private UserEntity user;
+
+    public CustomUserDetails(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
