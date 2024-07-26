151fa51f7c67b75ad0b673e1e3ecd0c5f6d5e1e4
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:23:44 +0900

    add UserEntity

--- src/main/java/org/example/testsecurity/entity/UserEntity.java
+++ src/main/java/org/example/testsecurity/entity/UserEntity.java
@@ -1,2 +1,22 @@
-package org.example.testsecurity.entity;public class UserEntity {
+package org.example.testsecurity.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
 }
