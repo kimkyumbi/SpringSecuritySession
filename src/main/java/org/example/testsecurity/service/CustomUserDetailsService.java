beef9f2a21da9a5571b96fb45c2e751dbf8f186b
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:22:17 +0900

    add CustomUserDetailsService

--- src/main/java/org/example/testsecurity/service/CustomUserDetailsService.java
+++ src/main/java/org/example/testsecurity/service/CustomUserDetailsService.java
@@ -1,2 +1,27 @@
-package org.example.testsecurity.service;public class CustomUserDetailsService {
+package org.example.testsecurity.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.testsecurity.dto.CustomUserDetails;
+import org.example.testsecurity.entity.UserEntity;
+import org.example.testsecurity.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity entity = userRepository.findByUsername(username);
+
+        if (entity != null) {
+            return new CustomUserDetails(entity);
+        }
+
+        return null;
+    }
 }
