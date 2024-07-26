1e96acd9fbeb7793cc457cb4023375bf998b8bf1
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:22:46 +0900

    add UserRepository

--- src/main/java/org/example/testsecurity/repository/UserRepository.java
+++ src/main/java/org/example/testsecurity/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package org.example.testsecurity.repository;public interface UserRepository {
+package org.example.testsecurity.repository;
+
+import org.example.testsecurity.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    boolean existByUsername(String username);
+
+    UserEntity findByUsername(String username);
 }
