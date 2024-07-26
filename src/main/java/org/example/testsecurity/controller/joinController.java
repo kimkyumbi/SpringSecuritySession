2a528f6d2b8c5b87e0a943a34f84894764d91179
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 18:22:54 +0900

    add joinController

--- src/main/java/org/example/testsecurity/controller/joinController.java
+++ src/main/java/org/example/testsecurity/controller/joinController.java
@@ -1,2 +1,29 @@
-package org.example.testsecurity.controller;public class joinController {
+package org.example.testsecurity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.testsecurity.dto.JoinDTO;
+import org.example.testsecurity.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class joinController {
+
+    private final JoinService joinService;
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @PostMapping("/joinProc")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+
+        return "redirect:/login";
+    }
 }
